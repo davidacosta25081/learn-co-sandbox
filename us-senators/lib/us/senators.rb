c2298b5cb42b22f3a5f04a853cfaac2682c3612c
David Acosta <davidacosta25081@yahoo.com>
2019-03-12 00:53:31 +0000

    first commit

---
+++ us-senators/lib/us/senators.rb
@@ -0,0 +1,8 @@
+require "us/senators/version"
+
+module Us
+  module Senators
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ us-senators/lib/us/senators/version.rb
@@ -0,0 +1,5 @@
+module Us
+  module Senators
+    VERSION = "0.1.0"
+  end
+end
